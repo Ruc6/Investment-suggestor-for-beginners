6ad9e212f117d82dad699f8a8a0258f61806993e
Rucha <rucharanade6@gmail.com>
2026-05-31 19:42:26 +0530

    Frontend fixes

---
+++ Frontend/script.js
@@ -0,0 +1,54 @@
+async function getRecommendation(){
+    const age=document.getElementById("age").value;
+    const age=document.getElementById("income").value;
+    const risk=document.getElementById("risk").value;
+    const investment_period=document.getElementById("period").value;
+    const response = await fetch(
+
+        "http://127.0.0.1:8000/recommend",
+
+        {
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+        
+    body:JSON.stringify({
+
+                age: Number(age),
+
+                income: Number(income),
+
+                risk: risk,
+
+                investment_period: Number(period)
+            })
+        });
+        const data = await response.json();
+document.getElementById("output").innerHTML = `
+
+        <h2>Financial Summary</h2>
+
+        <p>
+            Monthly Investment:
+            ${data.financial_summary.monthly_investment}
+        </p>
+
+        <h2>Allocation</h2>
+
+        <pre>
+${JSON.stringify(data.allocation, null, 2)}
+</pre>
+<h2> Recommendated stocks</h2>
+<p>
+${data.recommended_assets}
+</p>
+
+        <h2>AI Suggestion</h2>
+
+        <p>
+            ${data.suggestion}
+        </p>
+    `;
+}
